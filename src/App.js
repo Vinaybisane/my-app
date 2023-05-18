58294b44363f6aa6e9ad095f585ce8395e890873
Vinaybisane <vinaybisane@gmail.com>
2023-05-18 00:14:02 -0700

    my first commit

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>table</h1>
+   <table>
+    <tr>
+      <th>id</th>
+      <th>name</th>
+      <th>location</th>
+    </tr>
+    <tr>
+      <td>1</td>
+      <td>vinay</td>
+      <td>tumsar</td>
+    </tr>
+    <tr>
+      <td>2</td>
+      <td>Achal</td>
+      <td>nagpur</td>
+    </tr>
+   </table>
+
+   </div>
+  );
+}
+
+export default App;
